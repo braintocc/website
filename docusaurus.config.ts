bb1556b54435b44dc54d00ed6fa32fbb89f178dc
alvaro lorente <contact@alvarolorente.dev>
2024-06-16 16:50:37 +0200

    disable dark theme

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -49,6 +49,11 @@ const config: Config = {
   themeConfig: {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
+    colorMode: {
+      defaultMode: 'light',
+      disableSwitch: true,
+      respectPrefersColorScheme: false,
+    },
     navbar: {
       title: 'Brainto',
       logo: {
